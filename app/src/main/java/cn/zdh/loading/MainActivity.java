c6e31d5be2e3d1cb38cebf4b23a39b054f8e20e9
zhudaihao <zhudaihao@wswtz.com>
2018-09-04 10:59:22 +0800

    Initial commit

---
+++ app/src/main/java/cn/zdh/loading/MainActivity.java
@@ -0,0 +1,70 @@
+package cn.zdh.loading;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void send(View view) {
+        showProgressDialog("加载中...");
+    }
+
+
+    private Dialog customProgressDialog;
+
+    public void showProgressDialog(String message) {
+        if (customProgressDialog == null) {
+            customProgressDialog = createLoadingDialog(this, message);
+            // 设置点击屏幕Dialog不消失
+            customProgressDialog.setCanceledOnTouchOutside(false);
+            customProgressDialog.show();
+        }
+    }
+
+    /**
+     * 得到自定义的progressDialog
+     *
+     * @param context
+     * @param msg
+     * @return
+     */
+    public static Dialog createLoadingDialog(Context context, String msg) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.loading_dialog, null);// 得到加载view
+
+        LinearLayout layout = (LinearLayout) v.findViewById(R.id.dialog_view);// 加载布局
+        ImageView spaceshipImage = (ImageView) v.findViewById(R.id.img);
+        TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);// 提示文字
+
+        // 加载动画
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(context, R.anim.loading_animation);
+        // 使用ImageView显示动画
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+        tipTextView.setText(msg);// 设置加载信息
+
+        Dialog loadingDialog = new Dialog(context, R.style.loading_dialog);// 创建自定义样式dialog
+
+        loadingDialog.setCancelable(false);// 不可以用“返回键”取消
+        loadingDialog.setContentView(layout, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.FILL_PARENT));// 设置布局
+        return loadingDialog;
+    }
+}
